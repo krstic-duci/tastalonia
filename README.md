483d17b5559389bb882bdca3bfbf02fa19d62950
krstic-dusan <krstic.duci@gmail.com>
2020-07-21 21:27:26 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tastalonia
+Practice repo for react, redux (reduxjs/toolkit), react-router-dom
+
+# Steps:
+1. Install json-server via `npm install -g json-server` for fake API
+2. Run `npm start` for app init
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
